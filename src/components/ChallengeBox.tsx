d2d5100bf751262a15fb5ab612e3d87a4b0eca1e
Ester Gama <esterfaniasantos@gmail.com>
2021-02-24 23:07:48 -0300

    add challengeActiveComponent

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,11 @@
 import styles from '../styles/components/ChallengeBox.module.css';
+import { CountdownButton, CountdownButtonEnum } from './CountdownButton';
 
 export function ChallengeBox() {
   const hasActiveChallenge = true;
   return (
     <section className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? <ChallengeBoxNotActive /> : <div />}
+      {hasActiveChallenge ? <ChallengeBoxActive /> : <ChallengeBoxNotActive />}
     </section>
   );
 }
@@ -17,6 +18,36 @@ function ChallengeBoxNotActive() {
         <img src='icons/level-up.svg' alt='level up' />
         <figcaption> Avance de level completando os desafios.</figcaption>
       </figure>
+    </aside>
+  );
+}
+
+function ChallengeBoxActive() {
+  return (
+    <aside className={styles.challengeActive}>
+      <header>
+        <strong>Ganhe 400 xp</strong>
+      </header>
+      <main>
+        <figure>
+          <img src='icons/body.svg' alt='body' />
+          <figcaption>
+            <strong>Exercite-se</strong>
+            <p>
+              É agora Ester, bora lá minha parça. Caminhe por 3 minutos e
+              estique suas pernas pra você ficar saudável.
+            </p>
+          </figcaption>
+        </figure>
+      </main>
+      <footer>
+        <button type='button' className={styles.challengeFailedButton}>
+          Falhei
+        </button>
+        <button type='button' className={styles.challengeSucceededButton}>
+          Completei
+        </button>
+      </footer>
     </aside>
   );
 }
